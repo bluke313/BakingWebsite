8c158bf46d679afbde83ee9b36fb7b68fd303064
Luke Brooks <bluke313@vt.edu>
2024-11-10 11:06:01 -0500

    fixed security

--- frontend/src/AdminPage.js
+++ frontend/src/AdminPage.js
@@ -3,7 +3,7 @@ import { useState } from 'react';
 import './AdminPage.css';
 
 const AdminPage = () => {
-    const [authorized, setAuthorized] = useState(true);
+    const [authorized, setAuthorized] = useState(false);
     const [username, setUsername] = useState("");
     const [password, setPassword] = useState("");
     const [error, setError] = useState("");
